3be1a000073fd22cd6eaa5d0ee66376d09250803
Alan Daniel Alvarez Castro <alandanielalvarez0000@gmail.com>
2023-10-09 03:06:42 -0600

    feat: isolated logic into utils

---
+++ src/utils/index.tsx
@@ -0,0 +1,41 @@
+import { User } from '../types/userTypes';
+
+export const fetchData = async () => {
+    try {
+      const response = await fetch('https://randomuser.me/api/?results=100');
+      const { results: userData } = await response.json();
+      return userData;
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      return [];
+    }
+  };
+  
+  export const sortUsers = (
+    users: User[],
+    column: string,
+    sortAscending: boolean
+  ) => {
+    return [...users].sort((a, b) => {
+      const aValue = column === 'location.country'
+        ? a.location.country.toLowerCase()
+        : column === 'name.first'
+          ? a.name.first.toLowerCase()
+          : a.name.last.toLowerCase();
+  
+      const bValue = column === 'location.country'
+        ? b.location.country.toLowerCase()
+        : column === 'name.first'
+          ? b.name.first.toLowerCase()
+          : b.name.last.toLowerCase();
+  
+      return sortAscending ? aValue.localeCompare(bValue) : bValue.localeCompare(aValue);
+    });
+  };
+  
+  export const toggleRowColors = (rowColors: string[]) => {
+    return rowColors.map((_, index) =>
+      index % 2 === 0 ? '#808080' : '#D3D3D3'
+    );
+  };
+  
---
+++ src/utils/userUtils.test.ts
@@ -0,0 +1,83 @@
+import { User } from '../types/userTypes';
+import { fetchData, sortUsers, toggleRowColors } from './index';
+
+describe('fetchData', () => {
+  it('should fetch user data', async () => {
+    // Mock de datos de usuario ficticios
+    const mockUserData: User[] = [
+        {
+          name: {
+            first: 'John',
+            last: 'Doe',
+          },
+          location: {
+            country: 'USA',
+          },
+          picture: {
+            thumbnail: 'https://example.com/john.jpg',
+          },
+        },
+      ] as User[]; 
+      
+    
+    global.fetch = jest.fn().mockResolvedValue({
+      json: jest.fn().mockResolvedValue({ results: mockUserData }),
+    });
+
+    const userData = await fetchData();
+
+    expect(userData).toEqual(mockUserData);
+  });
+});
+
+describe('sortUsers', () => {
+  it('should sort users by name first in ascending order', () => {
+    const users: User[] = [
+      {
+        name: {
+          first: 'John',
+          last: 'Doe',
+        },
+        location: {
+          country: 'USA',
+        },
+        picture: {
+          thumbnail: 'https://example.com/john.jpg',
+        },
+      },
+      {
+        name: {
+          first: 'Alice',
+          last: 'Smith',
+        },
+        location: {
+          country: 'Canada',
+        },
+        picture: {
+          thumbnail: 'https://example.com/alice.jpg',
+        },
+      },
+    ];
+
+    // Ordena los usuarios por nombre en orden ascendente
+    const sortedUsers = sortUsers(users, 'name.first', true);
+
+    // Verifica que los usuarios estén ordenados correctamente por nombre
+    expect(sortedUsers[0].name.first).toBe('Alice');
+    expect(sortedUsers[1].name.first).toBe('John');
+  });
+});
+
+describe('toggleRowColors', () => {
+  it('should toggle row colors correctly', () => {
+    const rowColors: string[] = ['#808080', '#D3D3D3', '#808080'];
+
+    // Aplica la función para alternar colores
+    const newColors = toggleRowColors(rowColors);
+
+    // Verifica que los colores se alternen correctamente
+    expect(newColors[0]).toBe('#808080');
+    expect(newColors[1]).toBe('#D3D3D3');
+    expect(newColors[2]).toBe('#808080');
+  });
+});
